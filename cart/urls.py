e646bc214fc0b72caba194b1b6b1c490ccddd2bc
ConorBevan <conor.bevan@hotmail.com>
2023-07-13 19:14:31 +0000

    added edit and remove bag models need to be worked on

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('edit/<item_id>/', views.edit_cart_quantity, name='edit_cart_quantity'),
+    path('delete/<item_id>/', views.delete_from_cart, name='delete_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,24 +1,39 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, reverse, HttpResponse
 # Create your views here.
 
 
 def view_cart(request):
     """ A view that renders the cart contents page """
-
     return render(request, 'cart/cart.html')
 
 
 def add_to_cart(request, item_id):
-
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
-
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
-
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def edit_cart_quantity(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        del cart[item_id]
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def delete_from_cart(request, item_id):
+    cart = request.session.get('cart', {})
+    if item_id in cart:
+        del cart[item_id]
+    request.session['cart'] = cart
+    return HttpResponse(status=200)
