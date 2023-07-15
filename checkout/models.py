c8f67ca4062c768c833e78b5292503322c362587
ConorBevan <conor.bevan@hotmail.com>
2023-07-15 12:04:20 +0000

    created checkout models

---
+++ checkout/models.py
@@ -0,0 +1,54 @@
+import uuid
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+from products.models import Product
+
+# Create your models here.
+
+
+class Order(models.Model):
+    order_number = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, null=False, blank=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+    phone_number = models.CharField(max_length=25, null=False, blank=False)
+    country = models.CharField(max_length=40, default="Ireland", null=False, blank=False)
+    postcode = models.CharField(max_length=15, null=False, blank=False)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def update_total(self):
+        """
+        Update grand total & delivery cost each time a line item is added.
+        """
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = \
+                self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_colour = models.CharField(max_length=5, null=True, blank=True) # White or Black
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the save method to set the lineitem
+        total and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
