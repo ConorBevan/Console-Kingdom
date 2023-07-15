7339e27038eb9815db6bccae8acba929cd92fc19
ConorBevan <conor.bevan@hotmail.com>
2023-07-15 13:48:58 +0000

    added styling to checkout form and card layout using stripe basic formatting

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NU8UqLeBDUOjXVrLfNDNbjVQa5qovPNi0m6giq18VW9vB2jiAtoGFwTvF6He0uBXf01CRsIoBDLxnrQ49JumZbr00Efs25iw1',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
