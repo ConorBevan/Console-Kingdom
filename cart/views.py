a1f3892ca020a1ea256af76b9f686c169be295f1
ConorBevan <conor.bevan@hotmail.com>
2023-07-17 18:42:31 +0000

    toast success added but no feeback is being provided at the front end

--- cart/views.py
+++ cart/views.py
@@ -32,6 +32,7 @@ def add_to_cart(request, item_id):
             cart[item_id] += quantity
         else:
             cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
