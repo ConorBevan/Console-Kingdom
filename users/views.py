67edd13f9eac88c56b003538b0b09a10d6b26185
ConorBevan <conor.bevan@hotmail.com>
2023-07-20 14:20:52 +0000

    Tested that user sign ups work and user is visible when logged in

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 def user(request):
+    user = get_object_or_404(UserProfile, user=request.user)
 
     template = 'users/user.html'
-    context = {}
+    context = {
+        'user': user,
+    }
 
     return render(request, template, context)
