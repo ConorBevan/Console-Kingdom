79a0b2534496e729ac7ad0f10a73fcb7f53dbb33
ConorBevan <conor.bevan@hotmail.com>
2023-07-20 20:37:49 +0000

    Added user feedback

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 # Create your views here.
@@ -11,7 +11,7 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     colour = None
@@ -23,13 +23,17 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if colour in cart[item_id]['items_by_colour'].keys():
                 cart[item_id]['items_by_colour'][colour] += quantity
+                messages.success(request, f'Updated {product.name} quantity')
             else:
                 cart[item_id]['items_by_colour'][colour] = quantity
+                messages.success(request, f'Added {product.name} to your bag')
         else:
             cart[item_id] = {'items_by_colour': {colour: quantity}}
+            messages.success(request, f'Added {product.name} to your bag')
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity')
         else:
             cart[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
@@ -40,6 +44,7 @@ def add_to_cart(request, item_id):
 
 def edit_cart(request, item_id):
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     colour = None
     if 'product_colour' in request.POST:
@@ -49,15 +54,19 @@ def edit_cart(request, item_id):
     if colour:
         if quantity > 0:
             cart[item_id]['items_by_colour'][colour] = quantity
+            messages.success(request, f'Updated {product.name} quantity')
         else:
             del cart[item_id]['items_by_colour'][colour]
             if not cart[item_id]['items_by_colour']:
                 cart.pop(item_id)
+                messages.success(request, f'Removed {product.name} from your bag')
     else:
         if quantity > 0:
             cart[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -66,6 +75,7 @@ def edit_cart(request, item_id):
 def delete_from_cart(request, item_id):
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         colour = None
         if 'product_colour' in request.POST:
             colour = request.POST['product_colour']
@@ -75,11 +85,14 @@ def delete_from_cart(request, item_id):
             del cart[item_id]['items_by_colour'][colour]
             if not cart[item_id]['items_by_colour']:
                 cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
