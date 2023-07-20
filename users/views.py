538c489d759b4b5c96fd4b0bf3db2c22553e9b90
ConorBevan <conor.bevan@hotmail.com>
2023-07-20 15:39:33 +0000

    User account details page is working

--- users/views.py
+++ users/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def user(request):
     user = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Updated successfully')
+
+    form = UserProfileForm(instance=user)
+    orders = user.orders.all()
+
     template = 'users/user.html'
     context = {
-        'user': user,
+        'form': form,
+        'orders': orders,
+        'on_user_page': True
     }
 
     return render(request, template, context)
