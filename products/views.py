8eabb7d8f69240270988cae454f922a264b613fb
ConorBevan <conor.bevan@hotmail.com>
2023-07-21 12:23:18 +0000

    Added delete product functionality so all admin/superuser functionality is accessible in the front end

--- products/views.py
+++ products/views.py
@@ -28,9 +28,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product.')
     else:
@@ -64,3 +64,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product has been removed from the store!')
+    return redirect(reverse('products'))
